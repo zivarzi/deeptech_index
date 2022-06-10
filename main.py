5d025e7bc2d09f420a04484334dd6796f42da1bc
zivarzi <45911705+zivarzi@users.noreply.github.com>
2022-06-10 11:10:37 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import google_spreadsheet
+
+
+sheet_id = "1icwHkuyE9YUKMVwgwwdYM7iGJrKRV8cOBccCtN5UzV8"
+sheet_name = "רשימת חברות"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv" # &sheet={sheet_name}"
+
+df = pd.read_csv(url)
+
+
+st.title("Welcome to the Israeli Deep-tech company index")
+st.dataframe(df)
