147599b739d04607542266c2d4fa8d1249e4e457
Ziv Arzi <zivarzi@gmail.com>
2022-06-10 13:17:32 +0300

    added requirements

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
-
+import pillow
+import streamlit-aggrid
 
 sheet_id = "1icwHkuyE9YUKMVwgwwdYM7iGJrKRV8cOBccCtN5UzV8"
 sheet_name = "רשימת חברות"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv" # &sheet={sheet_name}"
 
 df = pd.read_csv(url)
-
 
 st.title("Welcome to the Israeli Deep-tech company index")
 st.dataframe(df)
