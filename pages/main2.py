0578c40095bdfdb3c7d7eafe0cb28565249fda48
Ziv Arzi <zivarzi@gmail.com>
2022-06-10 13:29:17 +0300

    bug fix

--- pages/main2.py
+++ pages/main2.py
@@ -6,8 +6,7 @@ import numpy as np
 import altair as alt
 from itertools import cycle
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
-import pillow
-import streamlit-aggrid
+
 
 np.random.seed(42)
 
